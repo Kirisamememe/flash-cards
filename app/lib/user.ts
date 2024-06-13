92e2711936889faf131883510d96b692411bccea
Kirisame <wuyuhuahen@gmail.com>
2024-06-13 14:28:34 +0900

    アニメーションを改善

--- app/lib/user.ts
+++ app/lib/user.ts
@@ -5,17 +5,17 @@ import { PrismaClient } from '@prisma/client/edge'
 const prisma = new PrismaClient()
 
 
-export const getUserByEmail = async (email: string) => {
-    try {
-        const user = await prisma.user.findUnique({
-            where: { email }
-        });
-        await prisma.$disconnect();
-        return user;
-    } catch {
-        return null;
-    }
-};
+// export const getUserByEmail = async (email: string) => {
+//     try {
+//         const user = await prisma.user.findUnique({
+//             where: { email }
+//         });
+//         await prisma.$disconnect();
+//         return user;
+//     } catch {
+//         return null;
+//     }
+// };
 
 export const getUserById = async (id: string) => {
     try {
