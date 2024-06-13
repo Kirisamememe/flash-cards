62353e8842de88d9f8391b2d26c2799f836214a3
Kirisame <wuyuhuahen@gmail.com>
2024-06-13 14:35:16 +0900

    アニメーションを改善

--- auth.config.ts
+++ auth.config.ts
@@ -1,29 +1,28 @@
-import bcrypt from 'bcryptjs';
-import type { NextAuthConfig } from 'next-auth';
-import Credentials from 'next-auth/providers/credentials';
-import { signInSchema } from './types';
-import {getUserByEmail} from "@/app/lib/user";
-
-export default {
-    providers: [
-        Credentials({
-            async authorize(credentials) {
-                const validatedFields = signInSchema.safeParse(credentials);
-
-                if (validatedFields.success) {
-                    const { email, password } = validatedFields.data;
-
-                    const user: any = await getUserByEmail(email);
-                    if (!user) {
-                        return null
-                    }else {
-                        const passwordsMatch = await bcrypt.compare(password, user.password);
-
-                        if (passwordsMatch) return user;
-                    }
-                }
-                return null;
-            },
-        }),
-    ],
-} satisfies NextAuthConfig;
+// import bcrypt from 'bcryptjs';
+// import type { NextAuthConfig } from 'next-auth';
+// import Credentials from 'next-auth/providers/credentials';
+// import { signInSchema } from './types';
+//
+// export default {
+//     providers: [
+//         Credentials({
+//             async authorize(credentials) {
+//                 const validatedFields = signInSchema.safeParse(credentials);
+//
+//                 if (validatedFields.success) {
+//                     const { email, password } = validatedFields.data;
+//
+//                     const user: any = await getUserByEmail(email);
+//                     if (!user) {
+//                         return null
+//                     }else {
+//                         const passwordsMatch = await bcrypt.compare(password, user.password);
+//
+//                         if (passwordsMatch) return user;
+//                     }
+//                 }
+//                 return null;
+//             },
+//         }),
+//     ],
+// } satisfies NextAuthConfig;
