39889873b67da5228dc244a08bc48ab5570ce62d
hanjunyi <wuyuhuahen@gmail.com>
2026-03-20 14:15:11 +0900

    fix: add trustHost

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -1,66 +1,59 @@
-import NextAuth from "next-auth"
-import "next-auth/jwt"
-import { PrismaAdapter } from '@auth/prisma-adapter';
+import NextAuth from "next-auth";
+import "next-auth/jwt";
+import { PrismaAdapter } from "@auth/prisma-adapter";
 // import bcrypt from 'bcryptjs';
 import GitHub from "@auth/core/providers/github";
-import { PrismaClient } from '@prisma/client/edge';
+import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 import Google from "@auth/core/providers/google";
 import Twitter from "@auth/core/providers/twitter";
 
-const prisma = new PrismaClient().$extends(withAccelerate())
+const prisma = new PrismaClient().$extends(withAccelerate());
 
 declare module "next-auth" {
-    interface Session {
-        accessToken?: string
-    }
+  interface Session {
+    accessToken?: string;
+  }
 }
 
 declare module "next-auth/jwt" {
-    interface JWT {
-        accessToken?: string
-    }
+  interface JWT {
+    accessToken?: string;
+  }
 }
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    adapter: PrismaAdapter(prisma),
-    providers: [
-        GitHub({
-            clientId: process.env.AUTH_GITHUB_ID,
-            clientSecret: process.env.AUTH_GITHUB_SECRET,
-        }),
-        Google({
-            clientId: process.env.AUTH_GOOGLE_ID,
-            clientSecret: process.env.AUTH_GOOGLE_SECRET,
-        }),
-        Twitter({
-            clientId: process.env.AUTH_TWITTER_ID,
-            clientSecret: process.env.AUTH_TWITTER_SECRET,
-            // version: "2.0", // opt-in to Twitter OAuth 2.0
-        })
-    ],
-    callbacks: {
-        authorized({ request, auth }) {
-            const { pathname } = request.nextUrl
-            if (pathname === "/user") return !!auth
-            return true
-        },
-        jwt({ token, trigger, session, account }) {
-            if (trigger === "update") token.name = session.user.name
-            if (account?.provider === "keycloak") {
-                return { ...token, accessToken: account.access_token }
-            }
-            return token
-        },
-        async session({ session, token }) {
-            if (token?.accessToken) {
-                session.accessToken = token.accessToken
-            }
-            return session
-        },
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+    }),
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+    Twitter({
+      clientId: process.env.AUTH_TWITTER_ID,
+      clientSecret: process.env.AUTH_TWITTER_SECRET,
+      // version: "2.0", // opt-in to Twitter OAuth 2.0
+    }),
+  ],
+  callbacks: {
+    authorized({ request, auth }) {
+      const { pathname } = request.nextUrl;
+      if (pathname === "/user") return !!auth;
+      return true;
     },
-    experimental: {
-        enableWebAuthn: true,
-    }
-})
-
+    async session({ session, token }) {
+      if (token?.accessToken) {
+        session.accessToken = token.accessToken;
+      }
+      return session;
+    },
+  },
+  experimental: {
+    enableWebAuthn: true,
+  },
+  trustHost: true,
+});
